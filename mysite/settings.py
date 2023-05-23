37604c6f5a1eb492ec9cdbe98f9cb967f5df84f6
SSStepa <svzshyklo@gmail.com>
2023-05-23 20:49:32 +0300

    UserModel added

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,3 +135,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "files", "media"),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "shop.UserModel"
--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import (
     CategoryModel,
     ProductModel,
+    UserModel,
 )
 
 admin.site.register(ProductModel)
 admin.site.register(CategoryModel)
+admin.site.register(UserModel, UserAdmin)
